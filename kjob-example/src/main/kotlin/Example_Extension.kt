42625e0c7a76703a998fc1446f4925ef095fa61a
Drew Carlson <awcarlson@protonmail.com>
2022-03-16 10:31:33 -0700

    Reformat Example_Extension.kt

--- kjob-example/src/main/kotlin/Example_Extension.kt
+++ kjob-example/src/main/kotlin/Example_Extension.kt
@@ -9,13 +9,16 @@ import kjob.core.kjob
 import kjob.inmem.InMem
 import kjob.inmem.InMemKJob
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
 
 // Generic Extension Example
 
 object MyGenericExtension : ExtensionId<MyGenericEx>
 
-class MyGenericEx(private val config: Configuration, private val kjobConfig: BaseKJob.Configuration, private val kjob: BaseKJob<BaseKJob.Configuration>) : BaseExtension(MyGenericExtension) {
+class MyGenericEx(
+    private val config: Configuration,
+    private val kjobConfig: BaseKJob.Configuration,
+    private val kjob: BaseKJob<BaseKJob.Configuration>
+) : BaseExtension(MyGenericExtension) {
     class Configuration : BaseExtension.Configuration() {
         var myGenericConfigValue = 1
     }
@@ -37,7 +40,10 @@ class MyGenericEx(private val config: Configuration, private val kjobConfig: Bas
 object MyGenericModule :
     ExtensionModule<MyGenericEx, MyGenericEx.Configuration, BaseKJob<BaseKJob.Configuration>, BaseKJob.Configuration> {
     override val id: ExtensionId<MyGenericEx> = MyGenericExtension
-    override fun create(configure: MyGenericEx.Configuration.() -> Unit, kjobConfig: BaseKJob.Configuration): (BaseKJob<BaseKJob.Configuration>) -> MyGenericEx {
+    override fun create(
+        configure: MyGenericEx.Configuration.() -> Unit,
+        kjobConfig: BaseKJob.Configuration
+    ): (BaseKJob<BaseKJob.Configuration>) -> MyGenericEx {
         return { MyGenericEx(MyGenericEx.Configuration().apply(configure), kjobConfig, it) }
     }
 }
@@ -46,7 +52,11 @@ object MyGenericModule :
 
 object MyInMemExtension : ExtensionId<MyInMemEx>
 
-class MyInMemEx(private val config: Configuration, private val inmemConfig: InMemKJob.Configuration, private val kjob: InMemKJob) : BaseExtension(MyInMemExtension) {
+class MyInMemEx(
+    private val config: Configuration,
+    private val inmemConfig: InMemKJob.Configuration,
+    private val kjob: InMemKJob
+) : BaseExtension(MyInMemExtension) {
     class Configuration : BaseExtension.Configuration() {
         var myInMemConfigValue = 1
     }
@@ -67,7 +77,10 @@ class MyInMemEx(private val config: Configuration, private val inmemConfig: InMe
 
 object MyInMemModule : ExtensionModule<MyInMemEx, MyInMemEx.Configuration, InMemKJob, InMemKJob.Configuration> {
     override val id: ExtensionId<MyInMemEx> = MyInMemExtension
-    override fun create(configure: MyInMemEx.Configuration.() -> Unit, kjobConfig: InMemKJob.Configuration): (InMemKJob) -> MyInMemEx {
+    override fun create(
+        configure: MyInMemEx.Configuration.() -> Unit,
+        kjobConfig: InMemKJob.Configuration
+    ): (InMemKJob) -> MyInMemEx {
         return { MyInMemEx(MyInMemEx.Configuration().apply(configure), kjobConfig, it) }
     }
 }
@@ -76,7 +89,11 @@ object MyInMemModule : ExtensionModule<MyInMemEx, MyInMemEx.Configuration, InMem
 
 object MyMongoExtension : ExtensionId<MyMongoEx>
 
-class MyMongoEx(private val config: Configuration, private val mongoConfig: MongoKJob.Configuration, private val kjob: MongoKJob) : BaseExtension(MyMongoExtension) {
+class MyMongoEx(
+    private val config: Configuration,
+    private val mongoConfig: MongoKJob.Configuration,
+    private val kjob: MongoKJob
+) : BaseExtension(MyMongoExtension) {
     class Configuration : BaseExtension.Configuration() {
         var myMongoConfigValue = 1
     }
@@ -97,7 +114,10 @@ class MyMongoEx(private val config: Configuration, private val mongoConfig: Mong
 
 object MyMongoModule : ExtensionModule<MyMongoEx, MyMongoEx.Configuration, MongoKJob, MongoKJob.Configuration> {
     override val id: ExtensionId<MyMongoEx> = MyMongoExtension
-    override fun create(configure: MyMongoEx.Configuration.() -> Unit, kjobConfig: MongoKJob.Configuration): (MongoKJob) -> MyMongoEx {
+    override fun create(
+        configure: MyMongoEx.Configuration.() -> Unit,
+        kjobConfig: MongoKJob.Configuration
+    ): (MongoKJob) -> MyMongoEx {
         return { MyMongoEx(MyMongoEx.Configuration().apply(configure), kjobConfig, it) }
     }
 }
