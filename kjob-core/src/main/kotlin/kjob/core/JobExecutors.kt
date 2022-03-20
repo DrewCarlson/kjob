2706585dc88ce99db67d8d8a4ddab883574ace38
Drew Carlson <awcarlson@protonmail.com>
2022-03-20 10:40:00 -0700

    Set name for non-blocking executor threads

--- kjob-core/src/main/kotlin/kjob/core/JobExecutors.kt
+++ kjob-core/src/main/kotlin/kjob/core/JobExecutors.kt
@@ -20,7 +20,9 @@ internal class DefaultJobExecutors(config: KJob.Configuration) : JobExecutors {
     override val executorService: ScheduledExecutorService by lazy { ScheduledThreadPoolExecutor(3) }
     override val dispatchers: Map<JobExecutionType, DispatcherWrapper> = mapOf(
             JobExecutionType.BLOCKING to object : DispatcherWrapper {
-                private val executor = Executors.newFixedThreadPool(config.blockingMaxJobs) { r -> Thread(r, "kjob-blocking-executor") } as ThreadPoolExecutor
+                private val executor = Executors.newFixedThreadPool(config.blockingMaxJobs) { r ->
+                    Thread(r, "kjob-blocking-executor")
+                } as ThreadPoolExecutor
                 override fun canExecute(): Boolean = executor.activeCount < executor.corePoolSize
                 override val coroutineDispatcher: CoroutineDispatcher = executor.asCoroutineDispatcher()
                 override fun shutdown() {
@@ -28,7 +30,12 @@ internal class DefaultJobExecutors(config: KJob.Configuration) : JobExecutors {
                 }
             },
             JobExecutionType.NON_BLOCKING to object : DispatcherWrapper {
-                private val executor = Executors.newWorkStealingPool(config.nonBlockingMaxJobs) as ForkJoinPool
+                private val threadFactory = ForkJoinPool.ForkJoinWorkerThreadFactory { pool ->
+                    ForkJoinPool.defaultForkJoinWorkerThreadFactory.newThread(pool).also { thread ->
+                        thread.name = "kjob-non-blocking-${thread.poolIndex}"
+                    }
+                }
+                private val executor = ForkJoinPool(config.nonBlockingMaxJobs, threadFactory, null, true)
                 override fun canExecute(): Boolean = executor.activeThreadCount < executor.parallelism
                 override val coroutineDispatcher: CoroutineDispatcher = executor.asCoroutineDispatcher()
                 override fun shutdown() {
